81b2e311462f148953e804e52b4af016399830a2
zhangtai <zhangtai@ybm100.com>
2023-03-25 19:12:22 +0800

    系统配置

--- src/views/SEO/index.vue
+++ src/views/SEO/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="components-container">
     <el-form :model="form" ref="form" label-width="120px" :rules="rules">
+      <aside>
+        网站的seo数据
+        <br />
+      </aside>
       <el-form-item label="网站名称" prop="title">
         <el-input
           v-model="form.title"
@@ -24,6 +28,91 @@
           maxlength="64"
         ></el-input>
       </el-form-item>
+      <aside>
+        数据存储相关的配置
+        <br />
+      </aside>
+      <el-form-item label="七牛云 AK" prop="qiniu_ak">
+        <el-input
+          v-model="form.qiniu_ak"
+          placeholder="请输入七牛云 AK"
+          maxlength="64"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="七牛云 SK" prop="qiniu_sk">
+        <el-input
+          v-model="form.qiniu_sk"
+          placeholder="请输入七牛云 SK"
+          maxlength="64"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="七牛云 Bucket" prop="qiniu_bucket">
+        <el-input
+          v-model="form.qiniu_bucket"
+          placeholder="请输入七牛云 Bucket"
+          maxlength="64"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="七牛云 Domain" prop="qiniu_domain">
+        <el-input
+          v-model="form.qiniu_domain"
+          placeholder="请输入七牛云 Domain"
+          maxlength="64"
+        ></el-input>
+      </el-form-item>
+
+      <aside>
+        在这里配置图片加密，建议开启，可跳过审查，防止被爬
+        <br />
+      </aside>
+      <el-form-item label="启用图片加密">
+        <el-switch
+          v-model="form.image_encryption_enabled"
+          active-text="启用"
+          inactive-text="禁用"
+        >
+        </el-switch>
+      </el-form-item>
+      <el-form-item label="图片加密 Key" prop="image_encryption_key">
+        <el-input
+          v-model="form.image_encryption_key"
+          placeholder="请输入图片加密 Key"
+          maxlength="64"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="图片加密 IV" prop="image_encryption_iv">
+        <el-input
+          v-model="form.image_encryption_iv"
+          placeholder="请输入图片加密 IV"
+          maxlength="64"
+        ></el-input>
+      </el-form-item>
+      <aside>
+        数据是否加密，请注意，如果开启数据加密，seo就没啥意义了。因为搜索引擎不知道网站是做什么的。
+        <br />
+      </aside>
+      <el-form-item label="启用数据加密">
+        <el-switch
+          v-model="form.data_encryption_enabled"
+          active-text="启用"
+          inactive-text="禁用"
+        ></el-switch>
+      </el-form-item>
+      <el-form-item label="数据加密 Key" prop="data_encryption_key">
+        <el-input
+          v-model="form.data_encryption_key"
+          placeholder="请输入数据加密 Key"
+          maxlength="64"
+        ></el-input>
+      </el-form-item>
+      <el-form-item label="数据加密 IV" prop="data_encryption_iv">
+        <el-input
+          v-model="form.data_encryption_iv"
+          placeholder="请输入数据加密 IV"
+          maxlength="64"
+        ></el-input>
+      </el-form-item>
+
       <el-form-item>
         <el-button type="primary" @click="submit" :loading="loading"
           >提交</el-button
